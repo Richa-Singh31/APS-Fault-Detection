c8fdaa8de38b7e3de212c082c196eecf93a4246b
Richa Singh <richarsingh31@gmail.com>
2024-09-12 14:07:09 +0530

    Data Ingestion and Validation

---
+++ sensor/pipeline/training_pipeline.py
@@ -0,0 +1,56 @@
+import os
+import sys
+from sensor.entity.config_entity import TrainingPipelineConfig, DataIngestionConfig, DataValidationConfig
+from sensor.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
+from sensor.exception import SensorException
+from sensor.logger import logging
+from sensor.components.data_ingestion import DataIngestion
+from sensor.components.data_validation import DataValidation
+
+class TrainPipeline:
+
+    def __init__(self):
+        self.training_pipeline_config = TrainingPipelineConfig()
+
+    def start_data_ingestion(self) -> DataIngestionArtifact:
+        try:
+            self.data_ingestion_config = DataIngestionConfig(training_pipeline_config=self.training_pipeline_config)
+
+            logging.info("Starting data ingestion")
+
+            data_ingestion = DataIngestion(data_ingestion_config=self.data_ingestion_config)
+
+            data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+
+            logging.info(f"Data ingestion completed and artifact: {data_ingestion_artifact}")
+            
+            return data_ingestion_artifact
+        
+        except Exception as e:
+            raise SensorException(e, sys) 
+        
+    def start_data_validation(self, data_ingestion_artifact: DataIngestionArtifact) -> DataValidationArtifact:
+        try:
+            
+            self.data_validation_config = DataValidationConfig(training_pipeline_config=self.training_pipeline_config)
+            logging.info("Data validation started")
+
+            data_validation = DataValidation(data_ingestion_artifact=data_ingestion_artifact,data_validation_config=self.data_validation_config)
+            logging.info("processing..")
+
+            data_validation_artifact = data_validation.initiate_data_validation()
+            logging.info("artifact constructed")
+
+            return data_validation_artifact
+        
+        except Exception as e:
+            return SensorException(e, sys)
+        
+    def run_pipeline(self):
+        try:
+            data_ingestion_artifact: DataIngestionArtifact = self.start_data_ingestion()
+
+            data_validation_artifact=self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+
+        except Exception as e:
+            raise SensorException(e, sys)
---
+++ sensor/utils/main_utils.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import yaml
+from sensor.exception import SensorException
+from sensor.logger import logging
+
+def read_yaml(file_path: str) -> dict:
+    try:
+        with open(file_path, "rb") as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logging.info(f"{content}")
+            return content
+        
+    except Exception as e:
+        raise SensorException(e, sys)
+
+
+
+    
+def write_yaml_file(file_path: str, content: object, replace: bool = False) -> None:
+    try:
+        if replace:
+            if os.path.exists(file_path):
+                os.remove(file_path)
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "w") as file:
+            yaml.dump(content, file)
+
+    except Exception as e:
+        raise SensorException(e, sys)
